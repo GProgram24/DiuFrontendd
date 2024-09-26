3b7d8daea700fe61c2e38d5549e71e20b5991e6f
avantiperla24 <avanti@bamboodigital.in>
2024-09-26 12:54:55 +0530

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     const closeDropdown = () => setDropdownOpen(false); // Close the dropdown function
 
     return (
-        <footer className='w-full min-h-[56px]  lg:px-[100px] xl:px-[150px] py-5 lg:py-0 flex items-center bg-yellow-100'>
+        <footer className='w-full min-h-[56px]  lg:px-[100px] xl:px-[150px] py-5 lg:py-0 flex items-center'>
             <div className='w-full h-full flex flex-col lg:flex-row justify-center items-center lg:justify-between gap-9 lg:gap-0'>
                 <nav className=' h-full'>
                     <div className='flex flex-wrap justify-center  gap-6 md:gap-16 lg:h-full font-subHeading text-[14px] md:text-base text-black  items-center'>
